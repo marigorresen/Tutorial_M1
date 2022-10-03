f91b11cd4cdd9d41553161dc5495d7748198cc1c
Mariana Görresen <110499531+marigorresen@users.noreply.github.com>
2022-10-03 10:16:41 -0300

    atividade ordenar numeros

---
+++ SEMANA_09/02_ATIVIDADES/atividade semana 9/Node2D.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+var lista = []
+var inserir
+var n 
+var copia
+		
+func _on_Button_pressed():
+	if len(lista) <= 9:
+		inserir = float($Button/LineEdit.text)
+		lista.append(inserir)
+	else:
+		$Button.visible = false
+		$Button/LineEdit.visible = false
+		$Label.text = "Voce ja inseriu 10 numeros"
+	
+func _on_Button2_pressed():
+	n = len(lista)
+	for j in range(n-1):
+		for i in range(n-1):
+			if lista[i] > lista[i+1]:
+				copia = lista[i]
+				lista[i] = lista[i+1]
+				lista[i+1] = copia
+	$Button2/ColorRect/Label2.text = str(lista)
